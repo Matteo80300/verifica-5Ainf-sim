3abbb8fcda40083f5e53c997e03fe507c7f04a68
Your Name <you@example.com>
2018-11-10 11:35:53 +0100

    Consegna Compito

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,7 +1,7 @@
 
 // TODO Inserire il package corretto
 // nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+package it.marconicivitavecchia.as2018_19.c5Binf.verifica;
 
 import javax.swing.*;
 
@@ -12,7 +12,7 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(500,300);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
 		
@@ -21,7 +21,7 @@ public class Main extends JFrame {
 		// TODO Convertire lo StringBuilder in String
 		
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(""));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
